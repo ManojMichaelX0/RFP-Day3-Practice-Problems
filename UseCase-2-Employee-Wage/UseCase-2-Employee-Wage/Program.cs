7c6db582096b3bea10ad1f33774fb78aba2d97cb
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-02-27 07:49:19 +0530

    added UseCase 2 Employee Wage Problem

---
+++ UseCase-2-Employee-Wage/UseCase-2-Employee-Wage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CalculateDailyWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int is_Present = 1;
+            int emp_Rate_Per_Hr = 20;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == is_Present)
+            {
+                empHrs = 8;
+
+
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * emp_Rate_Per_Hr;
+            Console.WriteLine("Emp Wage =" + empWage);
+        }
+    }
+}
