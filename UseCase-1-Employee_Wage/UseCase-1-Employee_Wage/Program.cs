9ab208a5784153541e5c343dbbce79f8032ef818
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-02-27 07:44:40 +0530

    added UseCase 1 Employee Program

---
+++ UseCase-1-Employee_Wage/UseCase-1-Employee_Wage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UseCase1_Employee_Wage_Calculation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            //Constants
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            //Computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("employee is Absent");
+            }
+        }
+    }
+}
